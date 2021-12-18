138b5c15772acd2da834465cbcc6f45aecf16de5
carlos_reyes <carlosarp2000@gmail.com>
2021-12-18 17:14:52 -0500

    configuracion de base de datos y uso de mongo

--- src/app.js
+++ src/app.js
@@ -1,10 +1,32 @@
 import express from "express";
-import indexRouter from "./routes/index.routes";
+import {create} from "express-handlebars";
+import indexRoutes from "./routes/index.routes";
+import path from "path";
+import morgan from "morgan";
 
 
 const app = express();
 
-app.use(indexRouter)
+
+app.set("views", path.join(__dirname, "views"));
+const exphbs = create({
+    extname: '.hbs',
+    layoutsDir: path.join(app.get("views"), "layouts"),
+    partialsDir: path.join(app.get("views"), "partials"),
+    defaultLayout:'main'
+  });
+  
+  
+app.engine(".hbs", exphbs.engine);
+app.set("view engine", ".hbs");
+
+// Middelwares
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false}));
+
+// routes
+app.use(indexRoutes);
 
 
-export default app
+export default app;
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,13 +1,26 @@
 import { Router} from 'express';
+import Task  from "../models/task";
 
 const router = Router()
 
 router.get("/", (req, res) => {
-    res.send("wold ");
+    res.render('index')
 });
 
+router.post('/task/add', async (req, res) => {
+    
+    const task = Task(req.body)
+    const taskSaverd = await task.save()
+    console.log(taskSaverd)
+    res.send('save')
+})
+
 router.get("/about", (req, res) => {
-    res.send("about us ");
+    res.render("about");
+});
+
+router.get("/edit", (req, res) => {
+    res.render("edit");
 });
 
 
