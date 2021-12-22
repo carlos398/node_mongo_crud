d359409034d4bac1581ea64f78850dc9e01350bb
carlos_reyes <carlosarp2000@gmail.com>
2021-12-21 23:26:52 -0500

    son configs

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import app from "./app";
 import './database'
 import {PORT} from "./config"
 
-app.listen(PORT);
-console.log("server on port", PORT);
+app.listen(PORT || 3000);
+console.log("server on port", PORT );
